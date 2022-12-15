33cae99ec4060e448ae6745c8c828e53f00121c7
miopio <mioakasako@gmail.com>
2022-12-14 20:13:54 -0500

    Now displays names nicely

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
   const colorsFromValues = [ 
-    ["Empowerment", "#0269E1"]
-  , ["Pride", "#0A2342"]
-  , ["Prejudice", "#6F34EE"]
-  , ["sdf", "#F68FFF"]
-  , ["sdfsdfsdf", "#FF6B6B"]
-  , ["1123123", "#FFBB0D"]
-  , ["sdxf", "#FFE380"]
-  , ["2342sd","#0DFF8B"]
-  , ["ppp", "#66EDFF"]
-  , ["xcvx", "#B1E3FF"]
-  , ["2349", "#D4DAFB"]
+    ["Empowerment", "#0269E1"] //ash electric blue
+  , ["Pride", "#0A2342"] //ash dark blue
+  , ["Prejudice", "#6F34EE"] //purple
+  , ["sdf", "#F68FFF"] //pink
+  , ["sdfsdfsdf", "#FF6B6B"] //red
+  , ["1123123", "#FFBB0D"] //orange-yellow
+  , ["sdxf", "#FFE380"] //yellow
+  , ["2342sd","#0DFF8B"] //green
+  , ["ppp", "#66EDFF"] //aqua
+  , ["xcvx", "#B1E3FF"] //light blue
+  , ["2349", "#D4DAFB"] //lavender
   ];
 
   const selectors = 
@@ -21,14 +21,18 @@
   function applyStyles() {
     // Apply the colors and styles to the blob elements
     var i = 1;
+    var colorStrings = [];
     for(selector of selectors){
       var selVal = document.getElementById(selector[0]).selectedOptions;
       if(selVal.length > 0){
         colorVal = colorsFromValues.filter(col => col[0] == selVal[0].value);
         document.getElementById('blob'+i).style.backgroundColor = colorVal[0][1];
+        colorStrings.push(colorVal[0][0]);
       }
       i = i + 1;
     }
+    var colorString = colorStrings.join(' + ');
+    document.getElementById("color-text").innerHTML = colorString;
   }
 
   // Input text into canvas
@@ -38,10 +42,6 @@
   document.getElementById("name-text-input").oninput = function () {
     document.getElementById("name-text").innerHTML = this.value;
   };
-  document.getElementById("color-text-input").oninput = function () {
-    document.getElementById("title-text").innerHTML = this.value;
-  };
-  
 
   function randomize() {
     // Get the dimensions of the canvas
@@ -53,15 +53,15 @@
     for(blob of blobs){
       // Set the positions of the blob elements to random coordinates
       const theBlob = document.getElementById(blob);
-      theBlob.style.top = Math.random()*canvasHeight/2 + "px";
+      theBlob.style.top = Math.random()*canvasHeight/20 + "px";
       theBlob.style.left = Math.random()*canvasWidth/2 + "px";
       // set radius
-      const blobRadius = (100 + Math.random()*200) + "px";
+      const blobRadius = (150 + Math.random()*200) + "px";
       theBlob.style.width = blobRadius;
       theBlob.style.height = blobRadius;
 
-      theBlob.style.opacity = 0.5+Math.random()*0.5;
-      var gaussianBlur = 20 + Math.random()*80;
+      //theBlob.style.opacity = 0.7+Math.random()*0.5;
+      var gaussianBlur = 10 + Math.random()*80;
       theBlob.style.filter = `blur(${gaussianBlur}px)`;
     }
   }
